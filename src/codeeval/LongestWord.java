07465e8aa76a2c73bd626aa24bf485cd752a05bb
Daven <daven1919@gmail.com>
2015-05-25 10:38:11 -0700

    finished challenged – efficiency in time possible, but not easily seeing it

--- src/codeeval/LongestWord.java
+++ src/codeeval/LongestWord.java
@@ -5,10 +5,37 @@
  */
 package codeeval;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author Daven
  */
 public class LongestWord {
-    
+    public static void main (String[] args) throws IOException {
+        File file = new File("src/longword.txt");
+        BufferedReader buffer = new BufferedReader(new FileReader(file));
+        String line;
+        
+        String[] words;
+        int key;
+        short length;
+        while ((line = buffer.readLine()) != null) {
+            key = 0;
+            length = 0;
+            line = line.trim();
+            words = line.split("\\s+");
+            
+            for (int i = 0; i<words.length; i++) {
+                if(words[i].length() > length){
+                    length = (short) words[i].length();
+                    key = i;
+                }
+            }
+            System.out.println(words[key]);
+        }
+    }
 }
